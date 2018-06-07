5e52bf7464f570155f08ae9586cdef9bf2715d84
Christinelj <345236126@qq.com>
2018-06-07 09:59:48 +0800

    'finish'

--- main/main.js
+++ main/main.js
@@ -1,4 +1,29 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(input) {
+    var Payment = 0;
+    var mileageCost = mileageCostCalculate(input);
+    var timeCost = timeCostCalculate(input);
+    Payment = Math.ceil(mileageCost + timeCost);
+
+    function mileageCostCalculate(input) {
+        var mileageCost = 0;
+        if (input.mileage <= 2) {
+            mileageCost = 6;
+        }
+        if (2 < input.mileage <= 8) {
+            mileageCost = Number(6 + (input.mileage - 2) * 0.);
+        }
+        if (input.mileage > 8) {
+            mileageCost = Number(6 + (8 - 2) * 0.8 + (input.mileage - 8) * 0.8 * 1.5);
+        }
+        return mileageCost;
+    }
+
+    function timeCostCalculate(input) {
+        var timeCost = 0;
+        timeCost = Number(input.time * 0.25);
+        return timeCost;
+    }
+
+    console.log(Payment);
+    return Payment;
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,15 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    var input;
+    beforeEach(function () {
+        input = { mileage: 10, time: 4 };
+    })
+
+    it('should print correct money', function () {
+        spyOn(console, 'log');
+        main(input);
+        var money = 15;
+        expect(console.log).toHaveBeenCalledWith(money);
+    })
 });
